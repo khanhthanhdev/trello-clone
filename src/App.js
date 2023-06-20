720ad1bd17aa5a62992bb8033a53abfe4e9ac6c9
khanhthanhdev <thanhkt27507@gmail.com>
2023-06-20 22:18:07 +0700

    add sass, add variable to use moretime

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
-import './App.css';
+import './App.scss';
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div className="trello-master">
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar app">Board Bar</nav>
+      <nav className="board-colums">Boards</nav>
     </div>
   );
 }
