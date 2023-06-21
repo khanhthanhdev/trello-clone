337a06c9a7ca858e1f5ec65085faf46206b90423
khanhthanhdev <thanhkt27507@gmail.com>
2023-06-21 09:15:08 +0700

    add simple layout

--- src/App.js
+++ src/App.js
@@ -4,8 +4,123 @@ function App() {
   return (
     <div className="trello-master">
       <nav className="navbar app">App Bar</nav>
-      <nav className="navbar app">Board Bar</nav>
-      <nav className="board-colums">Boards</nav>
+      <nav className="board app">Board Bar</nav>
+      <nav className="board-columns">
+        <div className="column">
+          <header>
+            Bran
+          </header>
+          <ul>
+            <li>
+              <img src="https://cdn.pixabay.com/photo/2017/06/26/13/03/webdesigner-2443766_640.jpg" alt="khanhthanhdev-alt-img" />
+              title: Khanhthanhdev
+            </li>
+            <li>Add what you'd like to work</li>
+            <li>Add what you'd like to work</li>
+            <li>Add what you'd like to work</li>
+            <li>Add what you'd like to work</li>
+            <li>Add what you'd like to work</li>
+          </ul>
+          <footer>
+            Add an other card
+          </footer>
+        </div>
+        <div className="column">
+          <header>
+            Bran
+          </header>
+          <ul>
+            <li>
+              <img src="https://cdn.pixabay.com/photo/2017/06/26/13/03/webdesigner-2443766_640.jpg" alt="khanhthanhdev-alt-img" />
+              title: Khanhthanhdev
+            </li>
+            <li>Add what you'd like to work</li>
+            <li>Add what you'd like to work</li>
+            <li>Add what you'd like to work</li>
+            <li>Add what you'd like to work</li>
+            <li>Add what you'd like to work</li>
+          </ul>
+          <footer>
+            Add an other card
+          </footer>
+        </div>
+        <div className="column">
+          <header>
+            Bran
+          </header>
+          <ul>
+            <li>
+              <img src="https://cdn.pixabay.com/photo/2017/06/26/13/03/webdesigner-2443766_640.jpg" alt="khanhthanhdev-alt-img" />
+              title: Khanhthanhdev
+            </li>
+            <li>Add what you'd like to work</li>
+            <li>Add what you'd like to work</li>
+            <li>Add what you'd like to work</li>
+            <li>Add what you'd like to work</li>
+            <li>Add what you'd like to work</li>
+          </ul>
+          <footer>
+            Add an other card
+          </footer>
+        </div>
+        <div className="column">
+          <header>
+            Bran
+          </header>
+          <ul>
+            <li>
+              <img src="https://cdn.pixabay.com/photo/2017/06/26/13/03/webdesigner-2443766_640.jpg" alt="khanhthanhdev-alt-img" />
+              title: Khanhthanhdev
+            </li>
+            <li>Add what you'd like to work</li>
+            <li>Add what you'd like to work</li>
+            <li>Add what you'd like to work</li>
+            <li>Add what you'd like to work</li>
+            <li>Add what you'd like to work</li>
+          </ul>
+          <footer>
+            Add an other card
+          </footer>
+        </div>
+        <div className="column">
+          <header>
+            Bran
+          </header>
+          <ul>
+            <li>
+              <img src="https://cdn.pixabay.com/photo/2017/06/26/13/03/webdesigner-2443766_640.jpg" alt="khanhthanhdev-alt-img" />
+              title: Khanhthanhdev
+            </li>
+            <li>Add what you'd like to work</li>
+            <li>Add what you'd like to work</li>
+            <li>Add what you'd like to work</li>
+            <li>Add what you'd like to work</li>
+            <li>Add what you'd like to work</li>
+          </ul>
+          <footer>
+            Add an other card
+          </footer>
+        </div>
+        <div className="column">
+          <header>
+            Bran
+          </header>
+          <ul>
+            <li>
+              <img src="https://cdn.pixabay.com/photo/2017/06/26/13/03/webdesigner-2443766_640.jpg" alt="khanhthanhdev-alt-img" />
+              title: Khanhthanhdev
+            </li>
+            <li>Add what you'd like to work</li>
+            <li>Add what you'd like to work</li>
+            <li>Add what you'd like to work</li>
+            <li>Add what you'd like to work</li>
+            <li>Add what you'd like to work</li>
+          </ul>
+          <footer>
+            Add an other card
+          </footer>
+        </div>
+      </nav>
     </div>
   );
 }
